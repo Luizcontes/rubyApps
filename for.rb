140d4d0bf26142de624acbab7bd51380b8c75725
Luiz <luiz.contes@hotmail.com>
2022-03-22 15:36:00 +0000

    Rep created while studying Ruby

---
+++ for.rb
@@ -0,0 +1,5 @@
+letters = ["A", "B", "C"]
+
+for i in 0..2 do
+    puts letters[i]
+end
---
+++ hello.rb
@@ -0,0 +1,32 @@
+puts "Welcome to 'Get My Number!'"
+print "What`s your name? "
+
+input = gets
+name = input.chomp
+
+puts "Welcome, #{name.capitalize}!"
+
+target = rand(100) + 1
+guessesNumber = 0
+
+while guessesNumber < 10
+    puts "Guess a number 0-100"
+    guess = gets.to_i
+    if guess == target 
+        puts "congratulations you got the right guess! #{target}"
+        break;
+    else
+        if guess < target
+            puts "LOW"
+        else
+            puts "HIGH"  
+        end
+    puts "#{10 - guessesNumber} guesses left.\n\n"
+    end
+    guessesNumber += 1;
+end
+
+if guessesNumber = 10
+    puts "You used all your chances!!!"
+end
+
